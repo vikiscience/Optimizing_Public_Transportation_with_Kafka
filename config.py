baea126075fa9e7ba45603f83da8c8a8f2387ca9
vikiscience <victoria.bibaeva@yandex.com>
2021-04-05 10:42:02 +0200

    refactor the Server

--- config.py
+++ config.py
@@ -9,7 +9,9 @@ KAFKA_CONNECT_URL = "http://localhost:8083"
 KSQL_URL = "http://localhost:8088"
 
 # topic names
+TOPIC_NAME_ARRIVAL = 'com.udacity.arrival'
 TOPIC_NAME_STATIONS = 'com.udacity.stations'
 TOPIC_NAME_TRANS_STATIONS = 'com.udacity.trans_stations'
 TOPIC_NAME_TURNSTILE = 'com.udacity.turnstile'
 TOPIC_NAME_TURNSTILE_SUMMARY = 'com.udacity.turnstile_summary'
+TOPIC_NAME_WEATHER = 'com.udacity.weather'
